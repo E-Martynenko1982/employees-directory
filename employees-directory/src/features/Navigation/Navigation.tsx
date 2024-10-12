c15782d30a9f36740d4b6a82f3a473a148fa2375
E-Martynenko1982 <motor8103@gmail.com>
2024-10-12 16:20:29 +0300

    update

--- employees-directory/src/features/Navigation/Navigation.tsx
+++ employees-directory/src/features/Navigation/Navigation.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';
 import Filter from './components/Filter/Filter';
 import Search from './components/Search/Search';
+import SignalIcon from "../../public/img/signal-icon.png";
+import WiFiIcon from "../../public/img/Wi-Fi.png";
+import BatteryIcon from "../../public/img/battery.png"
 import './index.scss';
 
 
@@ -16,9 +19,9 @@ const Navigation: React.FC = () => {
       <div className="header__icons-bar">
         <div className="header__time">9:41</div>
         <div className="header__icons">
-          <img src="../../public/img/signal-icon.png" alt="signal" />
-          <img src="../../public/img/Wi-Fi.png" alt="wi-fi" />
-          <img src="../../public/img/battery.png" alt="battery" />
+          <img src={SignalIcon} alt="signal" />
+          <img src={WiFiIcon} alt="wi-fi" />
+          <img src={BatteryIcon} alt="battery" />
         </div>
       </div>
       {!isEmployeesProfile && (
