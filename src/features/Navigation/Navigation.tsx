01244b9b09656ca6a572d3bc27279830d39496f5
E-Martynenko1982 <motor8103@gmail.com>
2024-10-17 18:55:46 +0300

    update

--- src/features/Navigation/Navigation.tsx
+++ src/features/Navigation/Navigation.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';
 import Filter from './components/Filter/Filter';
 import Search from './components/Search/Search';
-import SignalIcon from "../../../public/img/signal-icon.png";
-import WiFiIcon from "../../../public/img/Wi-Fi.png";
-import BatteryIcon from "../../../public/img/battery.png";
+// import SignalIcon from "/public/img/signal-icon.png";
+// import WiFiIcon from "public/img/Wi-Fi.png";
+// import BatteryIcon from "public/img/battery.png";
 import { useAppSelector } from '../../hooks/hooks';
 import useWindowWidth from '../../hooks/useWindowWidth';
 import './index.scss';
@@ -27,9 +27,9 @@ const Navigation: React.FC = () => {
         <div className="header__icons-bar">
           <div className="header__time">9:41</div>
           <div className="header__icons">
-            <img src={SignalIcon} alt="signal" />
-            <img src={WiFiIcon} alt="wi-fi" />
-            <img src={BatteryIcon} alt="battery" />
+            <img src="/img/signal-icon.png" alt="signal" />
+            <img src="/img/Wi-Fi.png" alt="wi-fi" />
+            <img src="/img/battery.png" alt="battery" />
           </div>
         </div>
       )}
