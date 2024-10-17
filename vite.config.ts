e0b593d116780f4dbd8c0146df661a08d5089661
E-Martynenko1982 <motor8103@gmail.com>
2024-10-17 13:30:11 +0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
-import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
-  root: 'src',
-  publicDir: 'public',
-  build: {
-    outDir: '../dist',
-    rollupOptions: {
-      input: resolve(__dirname, 'src/index.html'),
-    },
-  },
+  plugins: [react()],
 });
