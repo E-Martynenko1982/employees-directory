6b6bad991a4d21eda89709389ca8ed67c3bea349
E-Martynenko1982 <motor8103@gmail.com>
2024-10-19 20:42:00 +0300

    update

--- src/features/EmployeeProfile/EmployeesProfile.tsx
+++ src/features/EmployeeProfile/EmployeesProfile.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { useAppSelector } from '../../hooks/hooks';
 import { selectEmployeesData } from '../../redux/employeesSelectors';
