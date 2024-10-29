961f784063613d202995774d1e619d984b247a77
E-Martynenko1982 <motor8103@gmail.com>
2024-10-29 22:26:59 +0300

    update

--- src/features/Navigation/Navigation.tsx
+++ src/features/Navigation/Navigation.tsx
@@ -1,58 +1,70 @@
+// src/features/Navigation/Navigation.tsx
+
 import React from 'react';
-import { useLocation, matchPath } from 'react-router-dom';
 import useNetworkStatus from '../../hooks/useNetworkStatus';
+import { useLocation, matchPath } from 'react-router-dom';
+import useMediaQuery from '@mui/material/useMediaQuery';
+
+import "./index.scss";
 import Filter from './components/Filter/Filter';
 import Search from './components/Search/Search';
-import useWindowWidth from '../../hooks/useWindowWidth';
-import './index.scss';
 
 const Navigation: React.FC = () => {
-  const location = useLocation();
-  const isEmployeesProfile = matchPath('/employees/:id', location.pathname) !== null;
   const isOnline = useNetworkStatus();
-  const windowWidth = useWindowWidth();
-  const isLargeScreen = windowWidth > 1279;
+  const location = useLocation();
+  const isMobile = useMediaQuery('(max-width:1280px)');
+  const isProfilePage = matchPath('/employees/:id', location.pathname) !== null;
+
+  if (isProfilePage && !isMobile) {
+    return null;
+  }
 
   return (
-    <div className={`header ${isEmployeesProfile ? 'header__icons-bar--profile' : ''}`}>
-      <div className="header__icons-bar">
-        <div className="header__time">9:41</div>
-        <div className="header__icons">
-          <img src="/images/signal-icon.png" alt="signal" />
-          <img src="/images/Wi-Fi.png" alt="wi-fi" />
-          <img src="/images/battery.png" alt="battery" />
-        </div>
-      </div>
-
-      {!isOnline && (
-        <div className="header__network-error">
-          {isLargeScreen && <h4>Search</h4>}
-          <p>
-            Не могу обновить данные.
-            Проверь соединение с интернетом.
-          </p>
+    <header className={`header ${isProfilePage ? 'header--profile' : ''}`}>
+      {isProfilePage && isMobile ? (
+        <div></div>
+      ) : (
+        <div className="header__icons-bar">
+          <div className="header__time">9:41</div>
+          <div className="header__icons">
+            <img src="/images/signal-icon.png" alt="signal" />
+            <img src="/images/Wi-Fi.png" alt="wi-fi" />
+            <img src="/images/battery.png" alt="battery" />
+          </div>
         </div>
       )}
 
-      {isLargeScreen && isOnline && (
-        <div className="header__desktop-content">
-          <div className="header__desktop-title">Search</div>
-          <Search />
-        </div>
+      {!isProfilePage && (
+        <>
+          {!isOnline && (
+            <div className="header__network-error">
+              {isMobile ? <h4>Search</h4> : null}
+              <p>
+                Не могу обновить данные.
+                Проверь соединение с интернетом.
+              </p>
+            </div>
+          )}
+
+          {isMobile ? (
+            <div className="header__mobile-content">
+              <Search />
+            </div>
+          ) : (
+            isOnline && (
+              <div className="header__desktop-content">
+                <div className="header__desktop-title">Search</div>
+                <Search />
+              </div>
+            )
+          )}
+
+          <Filter />
+          <div className="header__line"></div>
+        </>
       )}
-
-      {!isLargeScreen && (
-        <div className="header__mobile-content">
-          <Search />
-        </div>
-      )}
-
-      <Filter />
-
-      <div className="header__line"></div>
-    </div>
+    </header>
   );
 };
 
 export default Navigation;
-
